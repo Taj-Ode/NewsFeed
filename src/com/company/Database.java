3703790011bc02cc49a553942a69ee7ad04c8094
Tajiana Odero <61429156+Taj-Ode@users.noreply.github.com>
2021-12-23 12:55:45 +0100

    Added database

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -1,4 +1,19 @@
 package com.company;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Database {
+
+    private Connection conn;
+
+    public Database() {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:NewsFeed.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import express.Express;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Database db = new Database();
+        Express app = new Express();
+
+
+        app.listen(3000);
+        System.out.println("Server started on port 3000");
     }
 }
